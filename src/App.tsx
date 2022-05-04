d3eb8beee11c6b3b629e1ad965651ca0edada6dd
kuhuisien <>
2022-05-04 17:11:50 +0800

    setup antd use

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { DatePicker } from "antd";
 
 const App = () => {
-  return <div className="App">test</div>;
+  return (
+    <div className="App">
+      <DatePicker />
+    </div>
+  );
 };
 
 export default App;
