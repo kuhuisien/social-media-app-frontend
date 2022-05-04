7c7019f63fe0eb4b708742990535dc59be107f8c
kuhuisien <>
2022-05-04 22:32:52 +0800

    refine Posts to render posts UI

--- src/ui/posts/Posts.tsx
+++ src/ui/posts/Posts.tsx
@@ -1,5 +1,63 @@
+import { gql, useQuery } from "@apollo/client";
+import { Key } from "react";
+import Spinner from "lib/components/Spinner/Spinner";
+import Post from "./Post/Post";
+import styles from "./Posts.module.css";
+import AppTitle from "lib/components/AppTitle/AppTitle";
+
+const GET_POSTS = gql`
+  query {
+    posts {
+      id
+      title
+      content
+      createdAt
+      user {
+        name
+      }
+    }
+  }
+`;
+
 const Posts = () => {
-  return <div>posts</div>;
+  const { data, error, loading } = useQuery(GET_POSTS);
+
+  console.log(error);
+
+  if (error) {
+    return <div>Error</div>;
+  }
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return (
+    <div>
+      <AppTitle displayText="Posts"></AppTitle>
+
+      <div className={styles.postsContainer}>
+        {data.posts.map(
+          (post: {
+            id: Key | null;
+            title: string | null;
+            content: string | null;
+            user: { name: string | null };
+            createdAt: string | null;
+          }) => (
+            <Post
+              key={post.id}
+              title={post.title}
+              content={post.content}
+              id={post.id}
+              username={post.user.name}
+              createdAt={post.createdAt}
+            />
+          )
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Posts;
