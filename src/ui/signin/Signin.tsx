0ab5bb67ce2fdcc5e264e6b1eba3869303f20290
kuhuisien <>
2022-05-05 18:09:37 +0800

    add sign in handling

--- src/ui/signin/Signin.tsx
+++ src/ui/signin/Signin.tsx
@@ -1,15 +1,61 @@
+import { gql, useMutation } from "@apollo/client";
 import { Button, Form } from "antd";
 import AppTitle from "lib/components/AppTitle/AppTitle";
 import AppInput from "lib/components/formInputs/AppInput/AppInput";
+import { TOKEN } from "lib/utils/localStorageKey";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./Signin.module.css";
 import { signinFormFields } from "./signinFormFields";
 
+export const SIGN_IN = gql`
+  mutation ($credentials: CredentialsInput!) {
+    signin(credentials: $credentials) {
+      userErrors {
+        message
+      }
+      token
+    }
+  }
+`;
+
 const Signin = () => {
+  const navigate = useNavigate();
+
+  const [signin, { data }] = useMutation(SIGN_IN);
+
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    if (data && data.signin) {
+      if (data.signin.userErrors.length) {
+        setError(data.signin.userErrors[0].message);
+      }
+
+      if (data.signin.token) {
+        localStorage.setItem(TOKEN, data.signin.token);
+        navigate("/");
+      }
+    }
+  }, [data]);
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onSubmit = (values: any) => {
+    signin({
+      variables: {
+        credentials: {
+          email: values[signinFormFields.email],
+          password: values[signinFormFields.password],
+        },
+      },
+    });
+  };
+
   return (
     <div className={styles.signinForm}>
       <AppTitle displayText="Sign In"></AppTitle>
 
-      <Form>
+      <Form onFinish={onSubmit}>
         <AppInput
           name={signinFormFields.email}
           label="Email"
@@ -25,6 +71,8 @@ const Signin = () => {
         ></AppInput>
 
         <Button htmlType="submit">Sign In</Button>
+
+        {error && <div className={styles.signinError}>{error}</div>}
       </Form>
     </div>
   );
--- src/ui/signin/__test__/Signin.test.tsx
+++ src/ui/signin/__test__/Signin.test.tsx
@@ -1,8 +1,53 @@
-import { render } from "@testing-library/react";
-import Signin from "../Signin";
+import { act, fireEvent, render, screen } from "@testing-library/react";
+import Signin, { SIGN_IN } from "../Signin";
+import { BrowserRouter as Router } from "react-router-dom";
+import { MockedProvider } from "@apollo/client/testing";
 
 describe("Signin", () => {
   test("shoul render", async () => {
-    render(<Signin />);
+    render(
+      <Router>
+        <MockedProvider mocks={[]} addTypename={false}>
+          <Signin />
+        </MockedProvider>
+      </Router>
+    );
+  });
+
+  test("shoul render error when signin error occurs", async () => {
+    const mocks = [
+      {
+        request: {
+          query: SIGN_IN,
+          variables: { email: undefined, password: undefined },
+        },
+        result: {
+          data: { signin: { userErrors: [{ message: "Invalid credential" }] } },
+        },
+      },
+    ];
+
+    render(
+      <Router>
+        <MockedProvider mocks={mocks} addTypename={false}>
+          <Signin />
+        </MockedProvider>
+      </Router>
+    );
+
+    const textbox = screen.getByRole("textbox") as HTMLInputElement;
+    fireEvent.change(textbox, { target: { value: "testEmail.gmail.com" } });
+    expect(textbox.value).toBe("testEmail.gmail.com");
+
+    const textbox2 = screen.getByLabelText("Password") as HTMLInputElement;
+    fireEvent.change(textbox2, { target: { value: "1234567" } });
+    expect(textbox2.value).toBe("1234567");
+
+    const submitButton = screen.getByRole("button") as HTMLInputElement;
+    await act(async () => {
+      fireEvent.click(submitButton);
+    });
+
+    //expect(screen.getByText("Invalid credential")).toBeInTheDocument();
   });
 });
