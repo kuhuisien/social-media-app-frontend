980ed4ea7056f86b7f5b08fb7eaa206725c31b53
kuhuisien <>
2022-05-05 18:09:05 +0800

    remove hardcoded "token" local storage key

--- src/lib/utils/apolloClient/apolloClient.ts
+++ src/lib/utils/apolloClient/apolloClient.ts
@@ -2,11 +2,12 @@ import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 import { GRAPHQL_URL_KEY } from "../env/keys";
 import { readEnv } from "../env/readEnv";
 import { setContext } from "@apollo/client/link/context";
+import { TOKEN } from "../localStorageKey";
 
 const httpLink = createHttpLink({ uri: readEnv(GRAPHQL_URL_KEY) });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem(TOKEN);
 
   return { headers: { ...headers, Authorization: token } };
 });
